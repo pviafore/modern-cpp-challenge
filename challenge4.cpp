b9294297dad36b8e57617bbe157970853a997985
Pat Viafore <patrick.viafore@adtran.com>
2018-11-01 16:01:19 -0500

    Challenge 4

---
+++ challenge4.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+auto sieve(unsigned int limit) {
+    std::vector<unsigned int> candidates(limit, 1u, std::vector<unsigned int>::allocator_type());
+    std::vector<unsigned int> out(limit, std::vector<unsigned int>::allocator_type()); 
+    for(auto i = 3u; i < limit; i += 2){
+       if(candidates[i] == 0) { continue; }
+        out.push_back(i);
+        for(auto j = 2*i; j < limit; j += i) {
+            candidates[j] = 0u;
+        }
+    }
+
+    return out;
+}
+
+int main() {
+    auto limit =0u;
+    std::cout << "Enter in your limit: ";
+    std::cin >> limit;    
+
+    std::cout << *sieve(limit).rbegin() << "\n";
+    return 0;
+}
