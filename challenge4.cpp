0e999c50b5a58fefc2cbbfca21fbd68143840ad9
Pat Viafore <patrick.viafore@adtran.com>
2018-11-05 16:22:44 -0600

    Challenge 5

--- challenge4.cpp
+++ challenge4.cpp
@@ -1,20 +1,5 @@
-#include <cmath>
+#include "prime.h"
 #include <iostream>
-#include <vector>
-
-auto sieve(unsigned int limit) {
-    std::vector<unsigned int> candidates(limit, 1u, std::vector<unsigned int>::allocator_type());
-    std::vector<unsigned int> out(limit, std::vector<unsigned int>::allocator_type()); 
-    for(auto i = 3u; i < limit; i += 2){
-       if(candidates[i] == 0) { continue; }
-        out.push_back(i);
-        for(auto j = 2*i; j < limit; j += i) {
-            candidates[j] = 0u;
-        }
-    }
-
-    return out;
-}
 
 int main() {
     auto limit =0u;
